09258cca0a1585538aa083b2d855ed4a96e701cb
Marc-André Appel <marcandre.appel@gmail.com>
2011-11-03 11:18:41 +0100

    Updated the version number.

--- controller.php
+++ controller.php
@@ -4,7 +4,7 @@ class SudimageRedirectionsPackage extends Package {
 
 	protected $pkgHandle = 'sudimage_redirections';
 	protected $appVersionRequired = '5.4.2';
-	protected $pkgVersion = '1.0.0';
+	protected $pkgVersion = '1.1';
 	
 	public function getPackageDescription() {
 		return t('Create simple redirections from a page in the sitemap to its child page.');
