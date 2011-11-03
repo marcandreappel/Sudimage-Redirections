9debabb66b7bbac6695baa87a1398bc19900edf4
Marc-André Appel <marcandre.appel@gmail.com>
2011-11-03 13:20:00 +0100

    Fixed bug with redirecting to system pages.

--- helpers/redirect.php
+++ helpers/redirect.php
@@ -3,25 +3,34 @@ defined('C5_EXECUTE') or die("Access Denied.");
 
 class RedirectHelper {
 
-	public function simpleRedirection() {
+	public function simpleRedirection()
+	{
 		$req = Request::get();
 		$_cID = $req->getCurrentPage()->cID;
 		$co = Collection::getByID($_cID);
 		if($co->getCollectionAttributeValue('redirect_page_to_child_page')) {
 			$c = Page::getByID($_cID);
-			if(is_object($c) && !$c->isError()) {
+			if ( is_object($c) && !$c->isError() )
+			{
+				$db = Loader::db();
 				$redirectpath = null;
 				$ca = array($c);
 				$i = 0;
-				while($redirectpath == null) {
-					if(!is_object($ca[$i])) {
+				while ( $redirectpath == null )
+				{
+					if ( !is_object($ca[$i]) )
+					{
 						header('Location: ' . BASE_URL . '/page_not_found');
 						exit;
 					}
-					$ca[($i+1)] = $ca[$i]->getFirstChild();
-					if(is_object($ca[($i+1)]) && !$ca[($i+1)]->isError) {
+					$r = $db->query("select cID from Pages where cParentID = ? order by cDisplayOrder asc", array($ca[$i]->cID));
+					$row = $r->fetchRow();
+					$ca[($i+1)] = Page::getByID($row['cID']);//$ca[$i]->getFirstChild();
+					if ( is_object($ca[($i+1)]) && !$ca[($i+1)]->isError )
+					{
 						$tco = Collection::getByID($ca[($i+1)]->cID);
-						if(!$tco->getCollectionAttributeValue('redirect_page_to_child_page')) {
+						if ( !$tco->getCollectionAttributeValue('redirect_page_to_child_page') )
+						{
 							$redirectpath = $ca[($i+1)]->cPath;
 						}
 					}
